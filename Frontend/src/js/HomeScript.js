a5f062f64b3eb2cc5a0ccf47f2fbb99fe72114cd
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-09-09 20:52:49 -0500

    aplicacion de three.js

--- Frontend/src/js/HomeScript.js
+++ Frontend/src/js/HomeScript.js
@@ -6,25 +6,17 @@ const finalColor = { r: 0, g: 0, b: 0 }; // Color final
 
 // Se ejecuta cuando el valor del input cambia
 rangeInput.addEventListener('input', function() {
-    /*if(rangeInput.value == 1 ){
-        document.body.style.backgroundColor = ` rgb(33, 50, 146)`;
-    }
-    else{
-        if(rangeInput.value == 2){
-            document.body.style.backgroundColor = ` rgb(0, 0, 0)`;
-        }
-    }*/
     const colorValue = rangeInput.value;
 
     document.querySelector('header').style.backgroundColor = interpolateColor(colorValue);
     //console.log(colorValue);
-    console.log(interpolateColor(colorValue));
+    //console.log(interpolateColor(colorValue));
   });
 
   function interpolateColor(value) { //Funcion de para cambiar de color progresivamente
     const r = Math.round(initialColor.r + (finalColor.r - initialColor.r) * (value / 100));
     const g = Math.round(initialColor.g + (finalColor.g - initialColor.g) * (value / 100));
     const b = Math.round(initialColor.b + (finalColor.b - initialColor.b) * (value / 100));
-    console.log("rgb");
+    //console.log("rgb");
     return `rgb(${r}, ${g}, ${b})`;
   }
---
+++ Frontend/src/js/backThree.js
@@ -0,0 +1,34 @@
+import * as THREE from "../../node_modules/three/build/three.module.js";
+
+document.addEventListener('DOMContentLoaded', function() {
+    const threeContainer = document.getElementById('three-container');
+    
+    //console.log(threeContainer.clientHeight);
+    
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, threeContainer.innerWidth / threeContainer.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( threeContainer.innerWidth, threeContainer.innerHeight );
+    //renderer.setClearColor(0x000000, 1);
+    threeContainer.appendChild( renderer.domElement );
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+
+    camera.position.z = 5;
+    scene.background = new THREE.Color(0xE83D02);
+
+    function animate() {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+        console.log("hola");
+        //console.log(threeContainer.clientWidth);
+    }
+
+    animate();
+});
