f40d70614154b3a8842106d769b7b60185fa1b11
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-09-10 20:37:31 -0500

    posicion del escenario 3d

--- Frontend/src/js/backThree.js
+++ Frontend/src/js/backThree.js
@@ -6,11 +6,11 @@ import * as THREE from "../../node_modules/three/build/three.module.js";
     //console.log(threeContainer.clientHeight);
     
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, threeContainer.innerWidth / threeContainer.innerHeight, 0.1, 1000 );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( threeContainer.innerWidth, threeContainer.innerHeight );
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer({alpha: true});
+    renderer.setSize( window.innerWidth, window.innerHeight );
     //renderer.setClearColor(0x000000, 1);
-    threeContainer.appendChild( renderer.domElement );
+    document.getElementById('three-container').appendChild( renderer.domElement );
 
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -18,7 +18,7 @@ import * as THREE from "../../node_modules/three/build/three.module.js";
     scene.add(cube);
 
     camera.position.z = 5;
-    scene.background = new THREE.Color(0xE83D02);
+    //scene.background = new THREE.Color(0xE83D02);
 
     function animate() {
         requestAnimationFrame(animate);
