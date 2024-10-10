3975cd82a3d945efbb124457fd0ecadbdb35374b
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-10-09 23:34:52 -0500

    correcion del slider. y su forma de mastrarse

--- Frontend/src/js/ScrollBehavior.js
+++ Frontend/src/js/ScrollBehavior.js
@@ -1,4 +1,4 @@
-const leftButton = document.querySelector("#scrolling-button_left");
+/*const leftButton = document.querySelector("#scrolling-button_left");
 const rightButton = document.querySelector("#scrolling-button_right");
 const worksContent = document.querySelector("#scrolling-container");
 
@@ -10,4 +10,45 @@ leftButton.addEventListener("click", function(){
 rightButton.addEventListener("click", function(){
     worksContent.scrollLeft += 800;//Mueve las tarjetas de trabajo hacia la derecha
     console.log("presionando boton 2");
+});*/
+
+const scrollingCards = document.querySelectorAll("#scrolling-card");
+const leftButton = document.querySelector("#scrolling-button_left");
+const rightButton = document.querySelector("#scrolling-button_right");
+const scrollingContainer = document.querySelector("#scrolling-container");
+let current = 3;
+const cardWidth = scrollingCards[0].offsetWidth; //obtiene el ancho de las tarjetas para centrarlas en la pantalla
+
+console.log(cardWidth);
+
+const offset = -current * cardWidth + (window.innerWidth / 2) - (cardWidth / 2);
+scrollingContainer.style.transform = `translateX(${offset}px)`;
+
+leftButton.addEventListener('click', function () {
+    moveTo(current - 1);
+    //worksContent.scrollLeft -= 400;
 });
+rightButton.addEventListener('click', function () {
+    moveTo(current + 1);
+    //worksContent.scrollLeft += 400;
+});
+
+function moveTo(index) {
+    if (index < 0) {
+        current = scrollingCards.length - 1; //para pasar del 0 al 6
+    } else if (index >= scrollingCards.length) {
+        current = 0; //para pasar del 6 al 1
+    } else {
+        current = index;
+    }
+
+    const offset = -current * cardWidth + (window.innerWidth / 2) - (cardWidth / 2);
+    scrollingContainer.style.transform = `translateX(${offset}px)`;
+
+    scrollingCards.forEach((card, i) => {
+        card.classList.remove('active');
+        if (i === current) {
+            card.classList.add('active');
+        }
+    });
+}
