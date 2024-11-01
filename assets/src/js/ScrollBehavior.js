8e168e5d275b5110283b2f114d9bba0c148fa1d8
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-11-01 00:08:36 -0500

    Detalles responsive

--- assets/src/js/ScrollBehavior.js
+++ assets/src/js/ScrollBehavior.js
@@ -1,20 +1,23 @@
 const scrollingCards = document.querySelectorAll("#scrolling-card"); //Genera un arreglo
 const bot = document.querySelectorAll("#dot");
-const leftButton = document.querySelector("#scrolling-button_left");
-const rightButton = document.querySelector("#scrolling-button_right");
+const leftButton = document.querySelectorAll("#scrolling-button_left");
+const rightButton = document.querySelectorAll("#scrolling-button_right");
 const scrollingContainer = document.querySelector("#scrolling-container");
 let cardSelected = 3;//La primera carta en mostrarse
 
 centerCard(cardSelected );
 
-leftButton.addEventListener('click', () => {
-    moveTo(cardSelected - 1);
-    console.log("Se mueve");
-});
-rightButton.addEventListener('click', () => {
-    moveTo(cardSelected + 1);
+leftButton.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        moveTo(cardSelected -1);
+    });
 });
 
+rightButton.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        moveTo(cardSelected + 1);
+    });
+}),
 
 //Estas lineas permiten centrar la tarjeta que se selecciona con le mouse
 scrollingCards.forEach(card => {
@@ -71,14 +74,16 @@ function centerCard(cardToCenter) {
 }
 /*-----------------------------RESPONSIVE------------------------------------*/
 const screenSize = screen.width;
-//console.log(screenSize);
-//console.log(screen.height);
 
 if (screenSize <= 650) {
     const scrollingButtonContainer = document.querySelectorAll("#scrolling-button_container");
-
+    console.log("hola");
     scrollingButtonContainer.forEach((button) => {
         button.classList.add("visible");
     });
+}
+if(screenSize > 650){
+    const buttonResponsive = document.querySelector('#button-responsive');
+    buttonResponsive.classList.add("visible");
 }
 
