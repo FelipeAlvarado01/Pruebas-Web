fb81592f2cbe4e2497b5b5fb27ab01f4fa6a6199
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-11-05 09:22:06 -0500

    uso de libreria typed para dinamismo en la letras

---
+++ assets/src/js/TypedController.js
@@ -0,0 +1,20 @@
+var typed = new Typed('#typed', {
+    strings: ['Hello, World!', 'System.out.println <i>("Hello, World!");</i>'],
+    typeSpeed: 70,
+    backSpeed: 80,
+    cursorChar: '_',
+    shuffle: true,
+    smartBackspace: false,
+    loop: false
+  });
+
+  
+  var typed = new Typed('#typed-text-request', {
+    strings: ['Thanks for visiting my website!'],
+    typeSpeed: 70,
+    backSpeed: 80,
+    cursorChar: '_',
+    shuffle: true,
+    smartBackspace: false,
+    loop: false
+  });
